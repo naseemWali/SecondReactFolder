30a3aab6a1d6c0c112face39ba43b4fd9f2e6c2e
naseem wali khan <naseemwalikhan776@gmail.com>
2024-10-12 13:09:54 -0700

    secondcommit

--- my-second-reactfolder/src/App.js
+++ my-second-reactfolder/src/App.js
@@ -3,6 +3,66 @@ import React, { Component } from 'react';
 import LifeCycle from './Componenets/LifeCycle';
 import RenderMethod from './Componenets/RenderMethod';
 
+
+//  class App extends Component {
+//    constructor(){
+//    super()
+//    console.log("hii");
+//    this.state={
+//     count:0,
+//    }
+//    // we can also pass props inside costructor like 
+//    // class App extends Component {
+//   //  constructor(props){
+//   //   super(props)
+//   //   console.log("hii");
+//   //   this.state={
+//   //    count:props.initialCount || 0,
+//   //   }
+//   // }
+
+//   //  console.log(this.stuff.count,"111");
+//    }
+
+//  componentDidMount(){
+//   console.log("i am componenetdidmounting");
+//  }
+
+
+//  componentDidUpdate(){
+//   console.log('i am componentdidupdate');
+  
+//  }
+  
+//  increment(){
+//   this.setState({count:this.state.count + 1});
+//   console.log('i am increment');
+//  }
+
+//   render() {
+//     return (
+//       <div>
+
+        
+//        <LifeCycle number={this.state.count}/> <LifeCycle/>
+        
+//         <button onClick={()=>{this.increment() }}>click me!</button> 
+//         <div>
+//           <h3>this  is render  component</h3>
+//          <RenderMethod/>
+//          </div>
+//       </div>
+
+//     )
+//   }
+// }
+
+
+
+
+
+// COMPONENTDIDMOUNT(){};
+
  class App extends Component {
    constructor(){
    super()
@@ -10,49 +70,85 @@ import RenderMethod from './Componenets/RenderMethod';
    this.state={
     count:0,
    }
-   // we can also pass props inside costructor like 
-   // class App extends Component {
-  //  constructor(props){
-  //   super(props)
-  //   console.log("hii");
-  //   this.state={
-  //    count:props.initialCount || 0,
-  //   }
-  // }
-
-  //  console.log(this.stuff.count,"111");
    }
 
  componentDidMount(){
-  console.log("componentDidMount : mounting");
+  console.log("this is componenetdidmount");
+ }
+ 
+ componentDidUpdate(prevprops,prevstate,snapShot){
+  console.log('this is componentdidupdate',prevstate); // here we use three parameters in componentdidupdate  prevstate tells us about prvious state not present.
+
+  // this.setState({count:this.state.count+1}); this makes infinite loop by directly setting state in this component.
+  // this is solution if we want to set state in this component then we have to  put it inside condition like
+  // if(this.state.count<10){ // so this will print up to 10
+
+
+  // }
+
+
+  if (prevstate.count===this.state.count) {
+    alert('same data');
+  }
+  //  we cant set state in componentdidupdate because it will go in infinite loop but we can do by putting condition like
+  
  }
 
-  
- increment(){
-  this.setState({count:this.state.count + 1});
-  console.log('i am increment');
-  
- }
 
+
+  
   render() {
+
+    console.log('render component');
+    
     return (
       <div>
         
-
        <LifeCycle number={this.state.count}/> <LifeCycle/>
         
         <button onClick={()=>{this.increment() }}>click me!</button> 
         <div>
           <h3>this  is render  component</h3>
+
+          
          <RenderMethod/>
+         
+         <div className='container mx-4 my-4' style={{width:'30vw',height:'30vh',border:'2px solid black',marginTop:'2%',marginLeft:'30%',borderRadius:'1rem'}}>
+          <h4 style={{marginLeft:'20%'}}>This is COMPONENTDIDMOUNT TOPIC</h4>
+          <h4 style={{marginLeft:'20%'}}> {this.state.count} </h4>
+           
+         <button style={{marginLeft:'30%',marginTop:'7%'}} onClick={()=>{this.setState({count:1})}}>click me to see changes!</button> 
+         {/* onClick={()=>{this.setState({count:this.state.count+1})}}  here above i make change count to count:1 because to show alert when prevState becomes equal to state*/}
+         
          </div>
-      </div>
-
-     
-
+         </div>
+         </div>
+      
     )
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
 
@@ -79,3 +175,20 @@ export default App;
 // When increment() is called inside console.log, it first increments count from 2 to 3 and returns 3.
 // This value is printed in the console.
 // Thus, the final output is 3.
+
+
+
+
+// let x = 5;
+// let y = x;
+// y = 10;
+// console.log(x);
+// 5 answer
+
+
+// let x = 5;
+// y = 10;
+// let y = x;
+// console.log(x);
+
+// we will get an error because of y is already been declare.
